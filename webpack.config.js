dbaaee0b38718bb936ed65cb3443511840dccf71
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-03-28 22:34:50 -0600

    bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: {
+    index:'./index.js',
+    historia:'./historia.js',
+    libros_populares:'./libros_populares.js',
+    nuevos_lanzamientos:'./nuevos_lanzamientos.js',
+    pag1:'./pags1.js',
+    pag2:'./pags2.js',
+    pag3:'./pags3.js'
+},
   output: {
     path: path.resolve(__dirname, 'dist_webpack'),
     filename: '[name].bundle.js',
@@ -10,8 +18,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
